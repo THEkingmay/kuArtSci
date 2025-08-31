ae3de4cdd5ba2a8b76272c0fc06dd1a82700e2bf
LetUsTakeANapForever <kandamkankaw@gmail.com>
2025-08-31 14:14:08 +0700

    add functions to handle fetching data, create and config buttons

--- src/pages/admin/Member.jsx
+++ src/pages/admin/Member.jsx
@@ -2,12 +2,15 @@ import { useState, useEffect } from "react";
 import { useAuth } from "../../context/AuthProvider";
 import AlertMessage from "../../components/AlertMessage";
 import SelectMemberRegisteraion from "../../components/admin/SelectRegister";
+import * as XLSX from "xlsx";
+import { saveAs } from "file-saver";
 
 export default function AdminMember() {
   const { API_URL, logout } = useAuth();
   const [memberRegisteration, setMemberRegisteration] = useState(null);
   const [alert, setAlert] = useState({ type: "", msg: "" });
   const [selectMember, setSelectMember] = useState(null);
+  const [approvedMemberRegistration, setApprovedMemberRegistration] = useState(null);
 
   const fetchData = async () => {
     const token = localStorage.getItem("token");
@@ -34,8 +37,51 @@ export default function AdminMember() {
   };
 
   useEffect(() => {
+    if (import.meta.env.DEV) {
+      localStorage.setItem("token", import.meta.env.VITE_DEV_TOKEN);
+    }
     fetchData();
   }, [API_URL]);
+
+  const fetchApprovedMemberRegistration = async () => {
+    const token = localStorage.getItem("token");
+    try {
+      const res = await fetch(`${API_URL}/member/getApprovedMemberRegistrations`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      const data = await res.json();
+      if (!res.ok) throw new Error(data.message);
+
+      setApprovedMemberRegistration(data.data);
+    } catch (err) {
+      console.log(err);
+      setAlert({
+        type: "error",
+        msg: err.message || "เกิดข้อผิดพลาดในการโหลดข้อมูล",
+      });
+    }
+  };
+
+
+  const exportToExcel = (data, fileName) => {
+    fetchApprovedMemberRegistration();
+
+    // Convert JSON to worksheet
+    const worksheet = XLSX.utils.json_to_sheet(data);
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, fileName);
+
+    // Write workbook to binary array
+    const excelBuffer = XLSX.write(workbook, { bookType: "xlsx", type: "array" });
+    const blob = new Blob([excelBuffer], { type: "application/octet-stream" });
+
+    saveAs(blob, fileName);
+  }
 
   return (
     <>
@@ -65,12 +111,29 @@ export default function AdminMember() {
             <h1 className="text-2xl font-bold text-gray-800">
               จัดการการสมัครเข้าร่วมสมาคมศิษย์เก่า
             </h1>
-            <button
-              onClick={logout}
-              className="mt-4 sm:mt-0 cursor-pointer underline underline-offset-8   px-5 py-2  hover:scale-105 hover:text-red-600 rounded-lg  transition-all duration-200 "
-            >
-              ออกจากระบบแอดมิน
-            </button>
+
+            {/* button group on the right */}
+            <div className="flex gap-4 mt-4 sm:mt-0">
+              <button
+                onClick={() => exportToExcel(approvedMemberRegistration, 'รายชื่อสมาชิกที่ยืนยันแล้ว.xlsx')}
+                className="px-5 py-2 bg-blue-500 text-white cursor-pointer rounded-lg shadow hover:bg-blue-900 hover:scale-105 transition-all duration-200"
+              >
+                Export รายชื่อที่ยืนยันแล้ว
+              </button>
+              <button
+                onClick={() => exportToExcel(memberRegisteration, 'รายชื่อสมาชิกที่สมัคร.xlsx')}
+                className="px-5 py-2 bg-blue-500 text-white cursor-pointer rounded-lg shadow hover:bg-blue-900 hover:scale-105 transition-all duration-200"
+              >
+                Export รายชื่อที่สมัครทั้งหมด
+              </button>
+
+              <button
+                onClick={logout}
+                className="px-5 py-2 underline underline-offset-8 cursor-pointer hover:scale-105 hover:text-red-600 rounded-lg transition-all duration-200"
+              >
+                ออกจากระบบแอดมิน
+              </button>
+            </div>
           </div>
 
           {/* Loading */}
@@ -100,19 +163,18 @@ export default function AdminMember() {
                       </p>
                     )}
                     <span
-                      className={`inline-block mt-2 px-3 py-1 text-xs font-medium rounded-full ${
-                        m.status === "pending"
-                          ? "bg-yellow-100 text-yellow-800"
-                          : m.status === "approved"
+                      className={`inline-block mt-2 px-3 py-1 text-xs font-medium rounded-full ${m.status === "pending"
+                        ? "bg-yellow-100 text-yellow-800"
+                        : m.status === "approved"
                           ? "bg-green-100 text-green-800"
                           : "bg-red-100 text-red-800"
-                      }`}
+                        }`}
                     >
                       {m.status === "pending"
                         ? "รอการยืนยัน"
                         : m.status === "approved"
-                        ? "อนุมัติแล้ว"
-                        : "ปฏิเสธแล้ว"}
+                          ? "อนุมัติแล้ว"
+                          : "ปฏิเสธแล้ว"}
                     </span>
                   </div>
 
